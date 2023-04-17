62a5dbaccec9045e2db1bd3cf135b626b7629089
Nihal Chaurasia <nihal@delimp.com>
2023-04-17 16:19:15 +0530

    Cancel Notification Popup dynamic

--- src/Common/Notifications.tsx
+++ src/Common/Notifications.tsx
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import IsLoadingHOC from "../Common/IsLoadingHOC";
+import { authAxios } from "../config/config";
+
+const Notifications= (props: any)=> {
+  const { setLoading } = props;
+  const [cancelData, setCancelData]= useState<any>([])
+  useEffect(()=>{
+    getCancelOrderNotifications();
+  },[])
 
 
-export default function Notifications() {
+const getCancelOrderNotifications = async () => {
+  setLoading(true);
+  await authAxios()
+    .get("/notification/get-cancel-order-notfications")
+    .then(
+      (response) => {
+        setLoading(false);
+        if (response.data.status === 1) {
+          console.log(response.data.data)
+          setCancelData(response.data.data)
+        } else {
+          toast.error(response.data.message);
+        }
+      },
+      (error) => {
+        setLoading(false);
+      }
+    )
+    .catch((error) => {
+      console.log("errorrrr", error);
+    });
+};
+
   return (
     <React.Fragment>
       <div className="notifications--dropdown">
         <div className="notifications--inner">
           <div className="notification--header">
-            <ul>
-              <li>
+          <ul>
+                <li>
                 <span>Notifications</span>
               </li>
               <li>
@@ -17,54 +49,25 @@ export default function Notifications() {
                 </div>
               </li>
             </ul>
+             
           </div>
           <div className="notification--body">
-            <ul>
-              <li>
-                <span className="icons">
-                  <i className="fa-sharp fa-solid fa-cart-shopping"></i>
-                </span>
-                <div className="notification--content">
-                  <span>user1 has Placed 2 order</span>
-                </div>
-                <div className="delete--notification">
-                  <i className="fa-sharp fa-solid fa-circle-xmark"></i>
-                </div>
-              </li>
-              <li>
-                <span className="icons">
-                  <i className="fa-sharp fa-solid fa-cart-shopping"></i>
-                </span>
-                <div className="notification--content">
-                  <span>user1 has Placed 2 order</span>
-                </div>
-                <div className="delete--notification">
-                  <i className="fa-sharp fa-solid fa-circle-xmark"></i>
-                </div>
-              </li>
-              <li>
-                <span className="icons">
-                  <i className="fa-sharp fa-solid fa-cart-shopping"></i>
-                </span>
-                <div className="notification--content">
-                  <span>user1 has Placed 2 order</span>
-                </div>
-                <div className="delete--notification">
-                  <i className="fa-sharp fa-solid fa-circle-xmark"></i>
-                </div>
-              </li>
-              <li>
-                <span className="icons">
-                  <i className="fa-sharp fa-solid fa-cart-shopping"></i>
-                </span>
-                <div className="notification--content">
-                  <span>user1 has Placed 2 order</span>
-                </div>
-                <div className="delete--notification">
-                  <i className="fa-sharp fa-solid fa-circle-xmark"></i>
-                </div>
-              </li>
-            </ul>
+          {cancelData && cancelData.map((item:any,index:any)=>(
+                <ul>
+                <li>
+                  <span className="icons">
+                    <i className="fa-sharp fa-solid fa-cart-shopping"></i>
+                  </span>
+                  <div className="notification--content">
+                    <span>Admin has cancelled your {item.order.products.length} order</span>
+                  </div>
+                  <div className="delete--notification">
+                    <i className="fa-sharp fa-solid fa-circle-xmark"></i>
+                  </div>
+                </li>
+              </ul>
+               ))}
+           
             <div className="read--all--notification">
               <a href="#" className="btn">
                 Mark All as Read
@@ -76,3 +79,5 @@ export default function Notifications() {
     </React.Fragment>
   );
 }
+
+export default IsLoadingHOC(Notifications);
